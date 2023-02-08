a61cc0ed4ab63d317f4533bee2f2eed9446c90aa
Vanessa Haefeli <vhaefeli@student.42lausanne.ch>
2023-02-08 18:01:23 +0100

    8 2 evening school

--- CPP_M07/ex00/include/whatever.hpp
+++ CPP_M07/ex00/include/whatever.hpp
@@ -15,7 +15,8 @@
 
 # include <iostream>
 
-template <typename T> void swap(T &a, T &b)
+template <typename T> 
+void swap(T &a, T &b)
 {
 	T	tmp;
 
@@ -24,14 +25,16 @@ template <typename T> void swap(T &a, T &b)
 	b = tmp;
 }
 
-template <typename T> T const &min(T const &a, T const &b)
+template <typename T> 
+T const &min(T const &a, T const &b)
 {
 	if (a < b)
 		return (a);
 	return (b);
 }
 
-template <typename T> T const &max(T const &a, T const &b)
+template <typename T> 
+T const &max(T const &a, T const &b)
 {
 	if (a > b)
 		return (a);
--- CPP_M07/ex00/src/main.cpp
+++ CPP_M07/ex00/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: vhaefeli <vhaefeli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/04 00:50:26 by vhaefeli          #+#    #+#             */
-/*   Updated: 2023/02/04 00:54:32 by vhaefeli         ###   ########.fr       */
+/*   Updated: 2023/02/08 14:59:57 by vhaefeli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,82 +15,22 @@
 #include <iostream>
 #include <cstdlib>
 
-void	convert(std::string s)
-{
-	// char *c;
-	(void)s;
-}
-
 int main ()
 {
-	std::srand (time(NULL));
+	int a = 2;
+	int b = 3;
 
-	std::cout << "--------- INTS ---------" << std::endl;
-	{
-		int	int1(std::rand() % 10000);
-		int	int2(std::rand() % 10000);
-
-		std::cout << "int1 = " << int1 << std::endl;
-		std::cout << "int2 = " << int2 << std::endl;
-		std::cout << "min = " << ::min(int1, int2) << std::endl;
-		std::cout << "max = " << ::max(int1, int2) << std::endl;
-		::swap( int1, int2);
-		std::cout << "swap !" << std::endl;
-		std::cout << "int1 = " << int1 << std::endl;
-		std::cout << "int2 = " << int2 << std::endl;
-	}
-	std::cout << "--------- EQUAL INTS ---------" << std::endl;
-	{
-		int	int1(std::rand() % 10000);
-		int	int2(int1);
-
-		std::cout << "Address of int1: " << &int1 << std::endl;
-		std::cout << "Address of int2: " << &int2 << std::endl;
-		std::cout << "Address of min(int1, int2): " << &::min(int1, int2) << std::endl;
-		std::cout << "Address of max(int1, int2): " << &::max(int1, int2) << std::endl;
-	}
-
-	std::cout << "--------- FLOATS ---------" << std::endl;
-	{
-		float float1(static_cast<float>(std::rand()) / static_cast <float> (RAND_MAX));
-		float float2(static_cast<float>(std::rand()) / static_cast <float> (RAND_MAX));
-
-		std::cout << "float1 = " << float1 << std::endl;
-		std::cout << "float2 = " << float2 << std::endl;
-		std::cout << "min = " << ::min(float1, float2) << std::endl;
-		std::cout << "max = " << ::max(float1, float2) << std::endl;
-		::swap( float1, float2);
-		std::cout << "swap !" << std::endl;
-		std::cout << "float1 = " << float1 << std::endl;
-		std::cout << "float2 = " << float2 << std::endl;
-	}
-	std::cout << "--------- DOUBLES ---------" << std::endl;
-	{
-		double double1(static_cast<double>(std::rand()) / static_cast <double> (RAND_MAX));
-		double double2(static_cast<double>(std::rand()) / static_cast <double> (RAND_MAX));
-
-		std::cout << "double1 = " << double1 << std::endl;
-		std::cout << "double2 = " << double2 << std::endl;
-		std::cout << "min = " << ::min(double1, double2) << std::endl;
-		std::cout << "max = " << ::max(double1, double2) << std::endl;
-		::swap( double1, double2);
-		std::cout << "swap !" << std::endl;
-		std::cout << "double1 = " << double1 << std::endl;
-		std::cout << "double2 = " << double2 << std::endl;
-	}
-	std::cout << "--------- STRINGS ---------" << std::endl;
-	{
-		std::string string1("Chocolat");
-		std::string string2("Tea");
-
-		std::cout << "string1 = " << string1 << std::endl;
-		std::cout << "string2 = " << string2 << std::endl;
-		std::cout << "min = " << ::min(string1, string2) << std::endl;
-		std::cout << "max = " << ::max(string1, string2) << std::endl;
-		::swap( string1, string2);
-		std::cout << "swap !" << std::endl;
-		std::cout << "string1 = " << string1 << std::endl;
-		std::cout << "string2 = " << string2 << std::endl;
-	}
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "Swap: " << std::endl;
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
 	return 0;
 }
